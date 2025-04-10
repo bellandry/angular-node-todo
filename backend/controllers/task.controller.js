047b40840301fb6873c336eea85f80ab34c9c409
bellandry <bellandry.work@gmail.com>
2025-04-10 19:52:41 +0100

    add login function & link tasks activities to logged user

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -2,7 +2,7 @@ const Task = require("../models/Task");
 const mongoose = require("mongoose");
 
 const getTasks = async (req, res) => {
-  const tasks = await Task.find();
+  const tasks = await Task.find({ userId: req.user.id });
   res.json(tasks);
 };
 
@@ -11,7 +11,7 @@ const createTask = async (req, res) => {
   if (!title || title.trim() == "") {
     return res.status(400).json({ message: "Title can't be empty." });
   }
-  const task = new Task(req.body);
+  const task = new Task({ ...req.body, userId: req.user.id });
   await task.save();
   res.status(201).json(task);
 };
@@ -20,36 +20,45 @@ const updateTask = async (req, res) => {
   const { title } = req.body;
   const { id } = req.params;
 
-  const taskExists = await taskExist(id, res);
+  const taskExists = await taskExist(req, res);
   if (!taskExists) return;
 
   if (!title || title.trim() == "") {
     return res.status(400).json({ message: "Title can't be empty." });
   }
-  const task = await Task.findByIdAndUpdate(id, req.body, {
-    new: true,
-  });
+  const task = await Task.findOneAndUpdate(
+    { _id: id, userId: req.user.id },
+    req.body,
+    {
+      new: true,
+    }
+  );
   res.json(task);
 };
 
 const deleteTask = async (req, res) => {
   const { id } = req.params;
 
-  const taskExists = await taskExist(id, res);
+  const taskExists = await taskExist(req, res);
   if (!taskExists) return;
 
-  await Task.findByIdAndDelete(id);
+  await Task.findOneAndDelete({ _id: id, userId: req.user.id });
   res.json({ message: "Task deleted" });
 };
 
-const taskExist = async (taskId, response) => {
-  if (!mongoose.Types.ObjectId.isValid(taskId)) {
+const taskExist = async (req, response) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return response.status(400).json({ message: "Invalid task ID." });
   }
 
-  const task = await Task.findById(taskId);
+  const task = await Task.findById(id);
   if (!task) {
     response.status(404).json({ message: "Task not found." });
+    return false;
+  }
+  if (task.userId != req.user.id) {
+    response.status(401).json({ message: "Access denied" });
     return false;
   }
   return true;
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   try {
@@ -19,4 +20,24 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { register, login };
