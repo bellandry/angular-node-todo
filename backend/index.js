bb074c47734037b0bf7bf8341c724f63e988f4a0
bellandry <bellandry.work@gmail.com>
2025-04-10 12:09:09 +0100

    create api task route manager

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { getTasks } = require("./controllers/task.controller");
 dotenv.config();
+
+const taskRoutes = require("./routes/task.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", async (req, res) => {
-  await getTasks(req, res);
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    console.log(`Status: ${res.statusCode}, Route: ${req.originalUrl}`);
+  });
+  next();
 });
+
+app.use("/api/tasks", taskRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
