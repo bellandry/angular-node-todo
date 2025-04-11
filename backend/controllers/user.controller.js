6508301f79256b8cd7ee34207c378e28ff961de3
bellandry <bellandry.work@gmail.com>
2025-04-11 08:20:03 +0100

    create getMe function on backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -40,4 +40,13 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { register, login };
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { register, login, getMe };
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { register, login } = require("../controllers/user.controller");
+const { register, login, getMe } = require("../controllers/user.controller");
+const { verifyToken } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/me", verifyToken, getMe);
 
 module.exports = router;
